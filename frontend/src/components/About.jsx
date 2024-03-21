fe1e8d66eb2766e1418106a4443bd649b5c40396
Ashutosh8357 <ashutoshtiwari.cse2024@gmail.com>
2024-03-21 23:57:18 +0530

    First_commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+   <section className= "about container ">
+
+  <h4>Events</h4>
+  <h2>ABOUT</h2>
+  <p>Welcome to RAX Event Planner, your premier destination for creating unforgettable moments and cherished memories. At RAX Event Planner , we specialize in curating exceptional experiences for weddings, birthday parties, camping adventures, anniversaries, and more. With a keen eye for detail and a passion for creativity, our dedicated team of event planners works tirelessly to bring your vision to life. From intimate gatherings to grand celebrations, we handle every aspect of planning and execution, ensuring a seamless and stress-free experience for you and your guests. Let us turn your dreams into reality. Contact us today to begin planning your next unforgettable event!
+
+  </p>
+
+
+   </section>
+  )
+}
+
+export default About
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="banner">
+        <div className="title">
+          <h1>RAX Event Planner </h1>
+          <p>Event and Weddings </p>
+        </div>
+        <div className="tag">
+          <label News Letter ></label>
+        <div>
+        <input type="text" placeholder='E-mail' />
+        <button>Subscribe</button>
+
+      </div>
+      <p>Sign Up with your email eddress to receive news and updates ! </p>
+      </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
